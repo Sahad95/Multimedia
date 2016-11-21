6b5d1be03d53e43f550476bda4acc236cb9e888a
Sahad95 <6f.arellano.ramirez.sahad@gmail.com>
2016-11-21 15:38:25 -0700

    Multimedia

---
+++ app/src/main/java/edu/tecii/android/multimedia/MainActivity.java
@@ -0,0 +1,89 @@
+package edu.tecii.android.multimedia;
+
+import android.media.MediaPlayer;
+import android.media.MediaRecorder;
+import android.os.Bundle;
+import android.os.Environment;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import java.io.File;
+import java.io.IOException;
+
+public class MainActivity extends AppCompatActivity {
+
+    TextView tv1;
+    MediaRecorder recorder;
+    MediaPlayer player;
+    File archivo;
+    Button b1, b2, b3;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        tv1 = (TextView) this.findViewById(R.id.txtMsg);
+        b1 = (Button) findViewById(R.id.record);
+        b2 = (Button) findViewById(R.id.stop);
+        b3 = (Button) findViewById(R.id.play);
+    }
+    public void grabar(View v) {
+        recorder = new MediaRecorder();
+        recorder.setAudioSource(MediaRecorder.AudioSource.MIC);
+        recorder.setOutputFormat(MediaRecorder.OutputFormat.THREE_GPP);
+        recorder.setAudioEncoder(MediaRecorder.AudioEncoder.AMR_NB);
+        File path = new File(Environment.getExternalStorageDirectory()
+                .getPath());
+        try {
+            archivo = File.createTempFile("temporal", ".3gp", path);
+        } catch (IOException e) {
+        }
+        recorder.setOutputFile(archivo.getAbsolutePath());
+        try {
+            recorder.prepare();
+        } catch (IOException e) {
+        }
+        recorder.start();
+        tv1.setText("Grabando");
+        b1.setEnabled(false);
+        b2.setEnabled(true);
+    }
+
+    public void detener(View v) {
+        recorder.stop();
+        recorder.release();
+        player = new MediaPlayer();
+        player.setOnCompletionListener((MediaPlayer.OnCompletionListener) this);
+        try {
+            player.setDataSource(archivo.getAbsolutePath());
+        } catch (IOException e) {
+        }
+        try {
+            player.prepare();
+        } catch (IOException e) {
+        }
+        b1.setEnabled(true);
+        b2.setEnabled(false);
+        b3.setEnabled(true);
+        tv1.setText("Listo para reproducir");
+    }
+
+    public void reproducir(View v) {
+        player.start();
+        b1.setEnabled(false);
+        b2.setEnabled(false);
+        b3.setEnabled(false);
+        tv1.setText("Reproduciendo");
+    }
+
+    public void onCompletion (MediaPlayer mediaPlayer) {
+        b1.setEnabled(true);
+        b2.setEnabled(true);
+        b3.setEnabled(true);
+        tv1.setText("Listo");
+    }
+
+}
